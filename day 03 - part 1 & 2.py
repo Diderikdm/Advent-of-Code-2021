fd9fe5e46803ee7f0aee5e6418c3b9e835f76600
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-03 14:59:27 +0100

    Update day 03 - part 1 & 2.py

--- day 03 - part 1 & 2.py
+++ day 03 - part 1 & 2.py
@@ -6,6 +6,7 @@ with open("2021 day3.txt", 'r') as file:
     data = file.read().splitlines()
     gamma = [get_value(x, 1, 0) for x in get_vertical(data)]
     epsilon = [int(not(x)) for x in gamma]
+    print(calc_bin(gamma) * calc_bin(epsilon)) 
     oxy = data[:]
     co2 = data[:]
     for i,j in enumerate(data[0]):
@@ -14,6 +15,5 @@ with open("2021 day3.txt", 'r') as file:
             oxy = [x for x in oxy if int(x[i]) == current_oxy]
         if len(co2) > 1:
             current_co2 = get_value(get_vertical(co2)[i], 0, 1)
-            co2 = [x for x in co2 if int(x[i]) == current_co2]
-    print(calc_bin(gamma) * calc_bin(epsilon))  
+            co2 = [x for x in co2 if int(x[i]) == current_co2] 
     print(calc_bin(oxy) * calc_bin(co2))
