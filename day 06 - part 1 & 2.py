61c8ee4305c8f8e257061d071d986d57fa7ddc83
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-06 10:37:43 +0100

    Update day 06 - part 1 & 2.py

--- day 06 - part 1 & 2.py
+++ day 06 - part 1 & 2.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 def calc(begin, end, data):
     for x in range(begin, end):
         temp = defaultdict(int)
-        temp[6], temp[8] = data[0], data[0]
+        temp[6] = temp[8] = data[0]
         for y in range(1, 9):
             temp[y - 1] += data[y]  
         data = {k:v for k,v in temp.items()}
