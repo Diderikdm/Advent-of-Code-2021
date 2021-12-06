b9265ce1117b64906347e502e55fbeae3c825bb1
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-06 10:30:25 +0100

    Create day 06 - part 1 & 2.py

---
+++ day 06 - part 1 & 2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+def calc(begin, end, data):
+    for x in range(begin, end):
+        temp = defaultdict(int)
+        temp[6], temp[8] = data[0], data[0]
+        for y in range(1, 9):
+            temp[y - 1] += data[y]  
+        data = {k:v for k,v in temp.items()}
+    return data
+
+with open("2021 day6.txt", 'r') as file:
+    data = defaultdict(int)
+    for x in file.read().split(','):
+        data[int(x)] += 1   
+    data = calc(0, 80, data)           
+    print(sum(data.values()))
+    print(sum(calc(80, 256, data).values()))
