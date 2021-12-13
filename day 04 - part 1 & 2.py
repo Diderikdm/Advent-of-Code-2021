529a293f13cb5e9a8be26a95aa46a9f26917ba79
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-13 11:50:04 +0100

    Update day 04 - part 1 & 2.py

--- day 04 - part 1 & 2.py
+++ day 04 - part 1 & 2.py
@@ -4,16 +4,13 @@ get_unmarked = lambda board: [x for x in sum([list(y) for y in board], []) if x 
 with open("2021 day4.txt", 'r') as file: 
     data = file.read().split('\n\n')
     numbers = [int(x) for x in data[0].split(',')]
-    boards = {}
-    
+    boards = {}  
     for e,x in enumerate(data[1:]):
         board = [[int(y) for y in row.split()] for row in x.splitlines()]
         vert_board = get_vertical(board)
         boards[e] = [tuple(sorted(x)) for x in board + vert_board]
-        
     play, rows, found_numbers, board_values, used_rows, used_boards = [], [], [], [], [], []
     all_rows = sum(boards.values(), [])
-    
     while len(used_boards) < len(boards):
         number = numbers.pop(0)
         play = tuple(sorted([x for x in play] + [number]))
@@ -23,7 +20,6 @@ with open("2021 day4.txt", 'r') as file:
                 if row in board and key not in used_boards:
                     used_boards.append(key)
                     used_rows.append(row)
-                    board_values.append(sum(set(get_unmarked(board))) * number)
-                        
+                    board_values.append(sum(set(get_unmarked(board))) * number)                     
     print(board_values[0])
     print(board_values[-1])
