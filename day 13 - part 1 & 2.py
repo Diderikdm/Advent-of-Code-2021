adcea6227f83923d3b6176455d142229a47b1417
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-13 10:07:22 +0100

    Update day 13 - part 1 & 2.py

--- day 13 - part 1 & 2.py
+++ day 13 - part 1 & 2.py
@@ -4,14 +4,14 @@ with open("2021 day13.txt", 'r') as file:
     for e, instruction in enumerate(folds):
         inst = int(instruction.split()[-1].split('=')[-1])
         if 'x' in instruction: 
-            to_fold = [x for x in grid.keys() if x[0] > inst]
+            to_fold = [x for x in grid if x[0] > inst]
         else:
-            to_fold = [x for x in grid.keys() if x[1] > inst]
-        for k in to_fold:
+            to_fold = [x for x in grid if x[1] > inst]
+        for x, y in to_fold:
             if 'x' in instruction: 
-                grid[(inst + (inst - k[0]), k[1])] = grid.pop(k)
+                grid[(inst + (inst - x), y)] = grid.pop((x,y))
             else:
-                grid[(k[0], inst + (inst - k[1]))] = grid.pop(k)
+                grid[(x, inst + (inst - y))] = grid.pop((x,y))
         if e == 0:
             print(len(grid))
     print('\n'.join([''.join([' ' if (x,y) not in grid else grid[(x,y)] for x in range(max(x[0] for x in grid)+1)]) for y in range(max(x[1] for x in grid)+1)]))
