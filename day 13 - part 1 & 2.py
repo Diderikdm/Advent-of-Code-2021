8dc86dadccfb6e33bd1d8ae247446791dfb699c8
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-13 09:48:15 +0100

    Update day 13 - part 1 & 2.py

--- day 13 - part 1 & 2.py
+++ day 13 - part 1 & 2.py
@@ -8,4 +8,4 @@ with open("2021 day13.txt", 'r') as file:
             grid[(inst + (inst - k[0]), k[1]) if 'x' in instruction else (k[0], inst + (inst - k[1]))] = grid.pop(k)
         if e == 0:
             print(len(grid))
-    print('\n'.join([''.join([' ' if (x,y) not in grid else grid[(x,y)] for x in range(max(x[0] for x in grid.keys())+1)]) for y in range(max(x[1] for x in grid.keys())+1)]))
+    print('\n'.join([''.join([' ' if (x,y) not in grid else grid[(x,y)] for x in range(max(x[0] for x in grid)+1)]) for y in range(max(x[1] for x in grid)+1)]))
