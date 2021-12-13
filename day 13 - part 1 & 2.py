2e89dfa6213c4ea10912b935349b6fc909211d8f
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-13 09:35:15 +0100

    Create day 13 - part 1 & 2.py

---
+++ day 13 - part 1 & 2.py
@@ -0,0 +1,11 @@
+with open("2021 day13.txt", 'r') as file:
+    data, folds = [x.splitlines() for x in file.read().split('\n\n')]
+    grid = {tuple(int(z) for z in y.split(',')) : '#' for y in data}
+    for e, instruction in enumerate(folds):
+        inst = int(instruction.split()[-1].split('=')[-1])
+        to_fold = [x for x in grid.keys() if x[0] > inst] if 'x' in instruction else [x for x in grid.keys() if x[1] > inst]
+        for k in to_fold:
+            grid[(inst + (inst - k[0]), k[1]) if 'x' in instruction else (k[0], inst + (inst - k[1]))] = grid.pop(k)
+        if e == 0:
+            print(len(grid))
+    print('\n'.join([''.join([' ' if (x,y) not in grid else grid[(x,y)] for x in range(max(x[0] for x in grid.keys())+1)]) for y in range(max(x[1] for x in grid.keys())+1)]))
