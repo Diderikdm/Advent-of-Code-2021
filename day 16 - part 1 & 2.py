70cb5c47e0728eb625171f5558169c24c1122ba2
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-16 13:37:12 +0100

    Update day 16 - part 1 & 2.py

--- day 16 - part 1 & 2.py
+++ day 16 - part 1 & 2.py
@@ -1,24 +1,22 @@
 from math import prod
 
-def parse_bin(string, values, val = '', op = None):
+def parse_bin(string, values, origin, val = '', op = None):
     v, t, string = int(string[:3], 2), int(string[3:6], 2), string[6:]
     versions.append(v)
     if t == 4:
         while op != '0':
             op, val, string = string[0], val + string[1:5], string[5:]
-        return string, values + [int(val, 2)]
+        return string, origin + values + [int(val, 2)]
     length = lengths[string[0]](string)
     if string[0] == '0':
         copy, string = string[16 : 16 + length], string[16 + length:]
         while len(copy) > 5:
-            copy, v = parse_bin(copy, [])
-            values += v
+            copy, values = parse_bin(copy, [], values)
     else:
         string = string[12:]
         for e in range(length):
-            string, v = parse_bin(string, [])
-            values += v
-    return string, [types[t](*values)]
+            string, values = parse_bin(string, [], values)
+    return string, origin + [types[t](*values)]
 
 with open("2021 day16.txt", 'r') as file:
     raw = file.read()
@@ -27,6 +25,6 @@ with open("2021 day16.txt", 'r') as file:
     lengths = {'0': lambda x: int(x[1:16], 2), '1': lambda x: int(x[1:12], 2)}
     types = {0:lambda *v:sum(v), 1:lambda *v:prod(v), 2:lambda *v:min(v), 3:lambda *v:max(v), 5:lambda x,y:int(x>y), 6:lambda x,y:int(x<y), 7:lambda x,y:int(x==y)}
     while len(data) > 11:
-        data, values = parse_bin(data, [])
+        data, values = parse_bin(data, [], [])
     print(sum(versions))
     print(values[0])
