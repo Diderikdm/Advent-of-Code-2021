d4399f8bce76f9bf9f20b9cfbe4719bb51a9bd18
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-17 08:10:03 +0100

    Create day 17 - part 1 & 2.py

---
+++ day 17 - part 1 & 2.py
@@ -0,0 +1,16 @@
+with open("2021 day17.txt", 'r') as file:
+    minx, maxx, miny, maxy = sum([[int(y) for y in x.split('=')[1].split('..')] for x in file.read().split(', ')], [])
+    count, best = 0, 0
+    for tx in range(1, maxx + 1):
+        for ty in range(miny, -maxy + maxx):
+            x, y, h = 0, 0, 0
+            vx, vy = tx, ty
+            while vx > 0 or y > miny:
+                x, y, h = x + vx, y + vy, max(h,y)
+                vx, vy = max(0, vx - 1), vy - 1
+                if minx <= x <= maxx and miny <= y <= maxy:
+                    count += 1
+                    best = max(best, h)
+                    break
+    print(best)
+    print(count)
