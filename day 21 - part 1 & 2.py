0e69083d80febc522a23b37f041977bd00c014b9
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-21 09:07:11 +0100

    Update day 21 - part 1 & 2.py

--- day 21 - part 1 & 2.py
+++ day 21 - part 1 & 2.py
@@ -5,7 +5,8 @@ def play_dirac_by_swap(current_player, other_player, current_score = 0, other_sc
     for turn, times in odds_of_rolling_results:
         current_position = (current_player + turn - 1) % 10 + 1
         loss, win = play_dirac_by_swap(other_player, current_position, other_score, current_score + current_position)
-        wins_current, wins_other = wins_current + times * win, wins_other + times * loss
+        wins_current = wins_current + times * win
+        wins_other = wins_other + times * loss
     return wins_current, wins_other
 
 with open("2021 day21.txt", 'r') as file:
@@ -24,4 +25,4 @@ with open("2021 day21.txt", 'r') as file:
         e += 1
     print(min(x[1] for x in players.values()) * rolls)
     odds_of_rolling_results = ((3,1), (4,3), (5,6), (6,7), (7,6), (8,3), (9,1))
-    print(max(play_dirac_by_swap(*data))) 
+    print(max(play_dirac_by_swap(*data)))
