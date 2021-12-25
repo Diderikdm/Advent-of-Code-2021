f7ade6e0f4052e873fd2c72bdadb9956b4206083
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-12-25 10:46:56 +0100

    Update day 25 - part 1 & 2.py

--- day 25 - part 1 & 2.py
+++ day 25 - part 1 & 2.py
@@ -1,16 +1,16 @@
 with open("2021 day25.txt", 'r') as file:
     data = file.read().splitlines()
-    grid = {(x,y) : data[y][x] for x in range(len(data[0])) for y in range(len(data))}
-    e, moved = 0, True
+    e, moved, maxx, maxy = 0, True, len(data[0]), len(data)
+    grid = {(x,y) : data[y][x] for x in range(maxx) for y in range(maxy)}
     while moved:
         moved = set()
-        for cucumber in [a for a in grid if grid[a] == '>' and grid[((a[0] + 1) % len(data[0]), a[1])] == '.']:
-            grid[((cucumber[0] + 1) % len(data[0]), cucumber[1])] = grid[cucumber]
-            grid[cucumber] = '.'
-            moved.add(((cucumber[0] + 1) % len(data[0]), cucumber[1]))
-        for cucumber in [a for a in grid if grid[a] == 'v' and grid[(a[0], (a[1] + 1) % len(data))] == '.']:
-            grid[(cucumber[0], (cucumber[1] + 1) % len(data))] = grid[cucumber]
-            grid[cucumber] = '.'
-            moved.add((cucumber[0], (cucumber[1] + 1) % len(data)))
+        for x,y in [(x,y) for x,y in grid if grid[x,y] == '>' and grid[((x + 1) % maxx, y)] == '.']:
+            grid[((x + 1) % maxx, y)] = grid[x,y]
+            grid[x,y] = '.'
+            moved.add((x,y))
+        for x,y in [(x,y) for x,y in grid if grid[x,y] == 'v' and grid[(x, (y + 1) % maxy)] == '.']:
+            grid[(x, (y + 1) % maxy)] = grid[x,y]
+            grid[x,y] = '.'
+            moved.add((x,y))
         e += 1
     print(e)
